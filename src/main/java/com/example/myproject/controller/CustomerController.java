d3cddccecda3cec08b54d3ae91182013fda0bce6
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-03-14 21:03:37 -0500

    added findall and findbyid end points

--- src/main/java/com/example/myproject/controller/CustomerController.java
+++ src/main/java/com/example/myproject/controller/CustomerController.java
@@ -1,15 +1,15 @@
 package com.example.myproject.controller;
 
+import com.example.myproject.model.Customer;
 import com.example.myproject.request.CustomerRequest;
 import com.example.myproject.response.BaseCustomerResponse;
 import com.example.myproject.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController // specialized annotation that incurs @Controller and @ResponseBody annotations
 @RequestMapping(value = "/store/v1/api") //used to add default url path
@@ -28,6 +28,28 @@ public class CustomerController {
             }
         }
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<BaseCustomerResponse> getCustomer(@PathVariable("id") Integer id) {
+        BaseCustomerResponse response = service.findById(id);
+        if(response != null) {
+            if (response.getResponse() != null) {
+                if (response.getResponse().getName() != null) {
+                    return new ResponseEntity<>(response, HttpStatus.FOUND);
+                }
+            }
+        }
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("customer")
+    public ResponseEntity<List<Customer>> getAllCustomer(){
+        List<Customer> customerList = service.getAllCustomer();
+        if(!customerList.isEmpty()){
+            return new ResponseEntity<>(customerList, HttpStatus.FOUND);
+        }
+        return new ResponseEntity<>(customerList, HttpStatus.NO_CONTENT);
     }
 
 }
