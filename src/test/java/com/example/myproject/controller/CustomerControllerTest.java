45429e5c6729c2f8b28fa85c16a079df40da0c01
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-03-14 21:15:54 -0500

    added all the test method for getCustomer & getAllCustomer

--- src/test/java/com/example/myproject/controller/CustomerControllerTest.java
+++ src/test/java/com/example/myproject/controller/CustomerControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.myproject.controller;
 
+import com.example.myproject.model.Customer;
 import com.example.myproject.request.CustomerRequest;
 import com.example.myproject.response.BaseCustomerResponse;
 import com.example.myproject.response.CustomerResponse;
@@ -10,18 +11,31 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Transactional
+@SpringBootTest
 class CustomerControllerTest {
 
     @Mock
     CustomerService service;
 
+    @Autowired
+    CustomerController cc;
+
     @InjectMocks
     CustomerController controller;
+
+
 
     @BeforeEach
     public void setup() {
@@ -46,6 +60,17 @@ class CustomerControllerTest {
         response.setPhone("3222331919");
         response.setAddress("123 Home Address, city, ST, 12200");
         return response;
+    }
+
+    private Customer getCustomer() {
+        Customer customer = new Customer();
+        customer.setCustomerId(1);
+        customer.setCustomerName("Jhon Doe");
+        customer.setCustomerEmail("jhon.doe@gmail.com");
+        customer.setCustomerPhone("3222331919");
+        customer.setCustomerAddress("123 Home Address, city, ST, 12200");
+        customer.setCustomerPassword("Password1!");
+        return customer;
     }
 
     private CustomerResponse getBadResponse() {
@@ -88,4 +113,41 @@ class CustomerControllerTest {
     }
 
 
+    @Test
+    void getCustomerTest() {
+        ResponseEntity<BaseCustomerResponse> baseResponse = getBaseResponse(getResponse());
+        Mockito.when((service.findById(Mockito.any()))).thenReturn(baseResponse.getBody());
+        ResponseEntity<BaseCustomerResponse> response = controller.getCustomer(123);
+        assertNotNull(response.getBody().getResponse());
+        assertEquals(baseResponse.getBody().getResponse().getId(), response.getBody().getResponse().getId());
+    }
+
+    @Test
+    void getCustomerExceptionTest(){
+        ResponseEntity<BaseCustomerResponse> baseResponse = getBaseResponse(getBadResponse());
+        Mockito.when(service.findById(0)).thenReturn(baseResponse.getBody());
+        ResponseEntity<BaseCustomerResponse> response = controller.getCustomer(0);
+        assertNull(response.getBody().getResponse().getId());
+        assertEquals(baseResponse.getBody().getResponse().getId(), response.getBody().getResponse().getId());
+    }
+
+    @Test
+    void getAllCustomerTest() {
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(getCustomer());
+        Mockito.when(service.getAllCustomer()).thenReturn(customerList);
+        cc.addCustomer(getRequest());
+        ResponseEntity<List<Customer>> cList = cc.getAllCustomer();
+        assertNotNull(cList);
+        assertEquals(customerList.get(0).getCustomerId(), cList.getBody().get(0).getCustomerId());
+    }
+
+    @Test
+    void getAllCustomerExceptionTest() {
+        List<Customer> customerList = new ArrayList<>();
+        Mockito.when(service.getAllCustomer()).thenReturn(customerList);
+        ResponseEntity<List<Customer>> cList = cc.getAllCustomer();
+        assertNotNull(cList);
+        assertTrue(cList.getBody().isEmpty());
+    }
 }
