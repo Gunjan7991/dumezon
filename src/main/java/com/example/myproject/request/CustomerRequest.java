1abd93055bf312080864eaff6ee84f2e7356a92b
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-03-14 21:17:12 -0500

    added overloaded constructor method

--- src/main/java/com/example/myproject/request/CustomerRequest.java
+++ src/main/java/com/example/myproject/request/CustomerRequest.java
@@ -10,9 +10,18 @@ import lombok.NoArgsConstructor;
 public class CustomerRequest {
     private String name;
     private String email;
+    private String old_password;
     private String password;
     private String re_password;
     private String address;
     private String phone;
 
+    public CustomerRequest(String name, String email, String password, String re_password, String address, String phone) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.re_password = re_password;
+        this.address = address;
+        this.phone = phone;
+    }
 }
